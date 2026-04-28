5efefae5e4aded12411b5defe58428f0f1bae531
Luna <luna.beatrizalves@gmail.com>
2026-04-28 20:52:45 -0300

    Deletando a pasta de DTO - Luna

--- ApiVendas/Controllers/EstoqueController.cs
+++ ApiVendas/Controllers/EstoqueController.cs
@@ -17,5 +17,45 @@ namespace ApiVendas.Controllers
             _context = context;
             _config = config;
         }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public IActionResult Listar()
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Os parâmetros da requisição são inválidos.");
+                }
+                if (_context.Estoque == null)
+                {
+                    return NotFound("Nenhum dado de sensor foi encontrado.");
+                }
+
+                return Ok(_context.Estoque);
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "Ocorreu um erro interno no banco de dados.");
+            }
+
+        }
     }
 }
