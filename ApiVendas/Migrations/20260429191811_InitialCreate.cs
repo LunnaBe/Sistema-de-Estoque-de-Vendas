2eb6bf712d3a6a5cc4b033c9a51a96b0516e7ce4
Luna <luna.beatrizalves@gmail.com>
2026-04-29 16:19:29 -0300

    Criação do banco de dados - Luna

---
+++ ApiVendas/Migrations/20260429191811_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiVendas.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estoque",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Codigo_Fornecedor = table.Column<string>(type: "TEXT", nullable: false),
+                    Nome_Produto = table.Column<string>(type: "TEXT", nullable: false),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false),
+                    Preco = table.Column<double>(type: "REAL", nullable: false),
+                    Data_Entrada = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Data_Saida = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estoque", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estoque");
+        }
+    }
+}
