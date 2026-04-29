5c75a164dd2a21a5e8b484d8b50b9b1d1f89f260
Luna <luna.beatrizalves@gmail.com>
2026-04-28 21:09:38 -0300

    Adicionando código da controller - Luna

--- ApiVendas/Controllers/EstoqueController.cs
+++ ApiVendas/Controllers/EstoqueController.cs
@@ -2,6 +2,7 @@
 using ApiVendas.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Shared;
 
 namespace ApiVendas.Controllers
 {
@@ -18,6 +19,34 @@ namespace ApiVendas.Controllers
             _config = config;
         }
 
+        /// <summary>
+        /// POST: api/v1/estoque - Cria um novo produto do estoque no banco de dados
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Requisitos de validação:
+        /// - Id: obrigatório, deve ser um número inteiro positivo
+        /// - Codigo_Fornecedor: obrigatório, deve ser uma string não vazia
+        /// - Nome_Produto: obrigatório, deve ser uma string não vazia
+        /// - Quantidade: obrigatório, deve ser um número inteiro positivo
+        /// - Preço: obrigatório, deve ser um número decimal, máximo de 2 casas decimais (ex: 40.00)
+        /// - Data_Entrada: obrigatório, deve ser uma data e hora válida no formato ISO 8601 (ex: 2024-06-01T12:00:00Z)
+        /// - Data_Saida: opcional, deve ser uma data e hora válida no formato ISO 8601 (ex: 2024-06-01T12:00:00Z)
+        /// 
+        /// 
+        /// Observação: O endpoint deve retornar um status de sucesso (200 OK) se o produto for criado com sucesso, 
+        /// ou um status de erro apropriado se a criação falhar devido a dados inválidos ou outros problemas.
+        /// </remarks>
+        /// 
+        /// <param name="produto"></param>
+        /// 
+        /// <returns></returns> 
+        /// <response code="200">Leituras dos produtos encontradas com sucesso</response>
+        /// <response code="204">Nenhum dado de produto disponível no momento</response>
+        /// <response code="400">Requisição inválida ou parâmetros malformados</response>
+        /// <response code="404">Nenhum registro de produto encontrado</response>
+        /// <response code="409">Conflito ao processar os dados dos produtos</response>
+        /// <response code="500">Erro interno do servidor</response>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status201Created)]
@@ -26,6 +55,43 @@ namespace ApiVendas.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CriarProduto([FromBody] EstoqueData produto)
+        {
+            try
+            {
+
+                if (produto.Quantidade > _config.Value.MaxQuantidade)
+                {
+                    return BadRequest("Quantidade acima do máximo permitido!");
+                }
+
+                if (produto.Quantidade > _config.Value.LimiteQuantidade)
+                {
+                    return BadRequest("Quantidade abaixo do limite permitido!");
+                }
+
+                if (produto.Id <= 0)
+                {
+                    return BadRequest("Id deve ser um número inteiro positivo.");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Os parâmetros da requisição são inválidos.");
+                }
+                if (produto == null)
+                {
+                    return NotFound("Nenhum produto foi encontrado no estoque.");
+                }
+                _context.Estoque.Add(produto);
+                _context.SaveChanges();
+                return Ok(produto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Ocorreu um erro interno no banco de dados.");
+            }
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -34,7 +100,22 @@ namespace ApiVendas.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 
-        public IActionResult Listar()
+        /// <summary>
+        /// GET: api/v1/estoque - Retorna uma lista de produtos de um estoque cadastrados no banco de dados
+        /// </summary>
+        /// 
+        /// <remarks> 
+        /// Uma requisição que retorna valores de um produto, como: Id, Codigo_Fornecedor, Nome_Produto, Quantidade, Preço, Data_Entrada, Data_Saida. 
+        /// 
+        /// Observação: O endpoint deve retornar uma lista de valores, 
+        /// ou um status de erro apropriado se nenhum valor for encontrado ou se a requisição for inválida.
+        /// </remarks>
+        /// <response code="200">Leituras dos produtos encontradas com sucesso</response>
+        /// <response code="204">Nenhum dado de produto disponível no momento</response>
+        /// <response code="400">Requisição inválida ou parâmetros malformados</response>
+        /// <response code="404">Nenhum registro de produto encontrado</response>
+        /// <response code="500">Erro interno do servidor</response>
+        public IActionResult ListarEstoque()
         {
             try
             {
@@ -44,7 +125,7 @@ namespace ApiVendas.Controllers
                 }
                 if (_context.Estoque == null)
                 {
-                    return NotFound("Nenhum dado de sensor foi encontrado.");
+                    return NotFound("Nenhum produto foi encontrado no estoque.");
                 }
 
                 return Ok(_context.Estoque);
