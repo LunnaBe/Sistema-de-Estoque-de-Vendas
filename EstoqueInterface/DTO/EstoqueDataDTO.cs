9f8e27a62be31bdb3a15b33ea22d0cae4a645c27
Luna <luna.beatrizalves@gmail.com>
2026-04-29 20:55:52 -0300

    Criando uma pasta context no EstoqueInterface - Luna

---
+++ EstoqueInterface/DTO/EstoqueDataDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiVendas.Models
+{
+    public class EstoqueDataDTO
+    {
+        public int Id { get; set; }
+        public string Codigo_Fornecedor { get; set; }
+        public string Nome_Produto { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+        public DateTime Data_Entrada { get; set; }
+        public DateTime Data_Saida { get; set; }
+    }
+}
