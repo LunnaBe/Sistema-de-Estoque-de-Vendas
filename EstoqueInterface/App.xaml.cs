382cc80801b767fff88842553c1ee490a6373190
Luna <luna.beatrizalves@gmail.com>
2026-04-29 21:45:01 -0300

    Criando a lógica do App.xaml, encaminhando o Database - Luna

--- EstoqueInterface/App.xaml.cs
+++ EstoqueInterface/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿
+using EstoqueInterface.Context;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,11 @@ namespace EstoqueInterface
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Database.Initialize();
+        }
     }
 
 }
